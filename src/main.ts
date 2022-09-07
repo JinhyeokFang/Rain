5993e02ab1e04383267ef259a603e4e6bc12395d
JinhyeokFang <jinhyeokfang@gmail.com>
2022-09-07 23:35:52 +0900

    make canvas size bigger

--- src/main.ts
+++ src/main.ts
@@ -5,18 +5,21 @@ import RainElementBuilder from "./builders/rain-element-builder";
 import WebCanvas from "./implements/web-canvas";
 
 const canvasElement: HTMLCanvasElement = <HTMLCanvasElement> document.getElementById('canvas');
-const canvas = new WebCanvas(500, 500, canvasElement);
+const canvas = new WebCanvas(window.innerWidth, window.innerHeight, canvasElement);
 
 function createRain() {
     const rainElement = new RainElementBuilder()
-    .setPosition({ x: Math.random() * 500, y: -50 })
-    .setId(0)
-    .setAcceleration(-10000)
-    .build();
+        .setPosition({ x: Math.random() * window.innerWidth, y: -50 })
+        .setId(0)
+        .setAcceleration(-10000)
+        .build();
+
+    canvasElement.width = window.innerWidth;
+    canvasElement.height = window.innerHeight;
 
     canvas.addElement(rainElement);
 }
 
-setInterval(createRain, 50);
+setInterval(createRain, 25);
 
 canvas.setUpdateInterval(10);
